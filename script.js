8d4f8c7e3be8914dd2cb1fede766ea7cfa6f5c05
Hamza Mir <mirhamzaatd@gmail.com>
2025-09-16 00:59:46 +0500

    Uodated: script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ class VisitorCounter {
             // Show loading state
             this.counterElement.classList.add('loading');
             
-            const response = await fetch('https://hamza-resume-function-c8fwfvgjf2a5cphv.eastasia-01.azurewebsites.net/api/visitor_counter', {
+            const response = await fetch('https://hamza-resume-function-c8fwfvgjf2a5cphv.eastasia-01.azurewebsites.net/api/visitors', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
